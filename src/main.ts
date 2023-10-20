079b3edd1466ddd89827d3a122f3b517f631e233
Nguyen Vu <phucnguyenvu0203@gmail.com>
2023-10-19 18:17:27 -0700

    Step 4 complete

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,9 @@ let lastX = 0;
 let lastY = 0;
 
 // Array to store the user's drawing points
+const displayList: [number, number][][] = [];
 const drawingPoints: [number, number][] = [];
+const redoStack: [number, number][][] = [];
 
 // Event listeners for mouse events to draw on the canvas
 canvas.addEventListener("mousedown", (e: MouseEvent) => {
@@ -51,9 +53,11 @@ canvas.addEventListener("mousemove", (e: MouseEvent) => {
 
 canvas.addEventListener("mouseup", () => {
   isDrawing = false;
-  // Dispatch the "drawing-changed" event with the drawing points
+  // Add the current line to the display list
+  displayList.push([...drawingPoints]);
+  // Dispatch the "drawing-changed" event with the display list
   canvas.dispatchEvent(
-    new CustomEvent("drawing-changed", { detail: drawingPoints })
+    new CustomEvent("drawing-changed", { detail: displayList })
   );
   // Clear the drawing points after dispatching the event
   clearDrawingPoints();
@@ -96,10 +100,63 @@ clearButton.addEventListener("click", () => {
   }
 });
 
+// Create and add the "Undo" button
+const undoButton = document.createElement("button");
+undoButton.innerHTML = "Undo";
+undoButton.id = "undoButton";
+horizontalContainer.append(undoButton);
+
+// Create and add the "Redo" button
+const redoButton = document.createElement("button");
+redoButton.innerHTML = "Redo";
+redoButton.id = "redoButton";
+horizontalContainer.append(redoButton);
+
+// Undo button functionality
+undoButton.addEventListener("click", () => {
+  if (ctx) {
+    undo();
+  }
+});
+
+// Redo button functionality
+redoButton.addEventListener("click", () => {
+  if (ctx) {
+    redo();
+  }
+});
+
+// Function to undo the last line
+function undo() {
+  if (displayList.length > 0) {
+    const line = displayList.pop(); // Remove the last line from displayList
+    if (line) {
+      redoStack.push(line); // Add the line to redoStack
+      canvas.dispatchEvent(
+        new CustomEvent("drawing-changed", { detail: displayList })
+      );
+    }
+  }
+}
+
+// Function to redo the last undone line
+function redo() {
+  if (redoStack.length > 0) {
+    const line = redoStack.pop(); // Remove the last line from redoStack
+    if (line) {
+      displayList.push(line); // Add the line back to displayList
+      canvas.dispatchEvent(
+        new CustomEvent("drawing-changed", { detail: displayList })
+      );
+    }
+  }
+}
+
 // Function to clear the canvas
 function clearCanvas() {
   if (ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    displayList.length = 0;
     clearDrawingPoints();
   }
 }
@@ -112,35 +169,26 @@ function clearDrawingPoints() {
 // Observer for the "drawing-changed" event
 canvas.addEventListener("drawing-changed", (e: Event) => {
   if (ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the entire canvas
+
     // Type assertion to specify the event detail's type
-    const customEvent = e as CustomEvent<[number, number][]>;
-    const points: [number, number][] = customEvent.detail;
+    const customEvent = e as CustomEvent<[number, number][][]>; // Make sure it's an array of arrays
+    const displayList: [number, number][][] = customEvent.detail;
 
-    // Clear only the last drawn line
-    if (points.length >= 2) {
-      const [x1, y1] = points[points.length - 2];
-      const [x2, y2] = points[points.length - 1];
-
-      ctx.clearRect(
-        Math.min(x1, x2),
-        Math.min(y1, y2),
-        Math.abs(x2 - x1),
-        Math.abs(y2 - y1)
-      );
+    // Redraw the entire drawing from displayList
+    for (const points of displayList) {
+      ctx.beginPath();
+      points.forEach(([x, y], index) => {
+        if (index === 0) {
+          ctx.moveTo(x, y);
+        } else {
+          ctx.lineTo(x, y);
+        }
+      });
+      ctx.strokeStyle = "black";
+      ctx.lineWidth = 2;
+      ctx.lineCap = "round";
+      ctx.stroke();
     }
-
-    // Redraw the entire drawing
-    ctx.beginPath();
-    points.forEach(([x, y], index) => {
-      if (index === 0) {
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
-      }
-    });
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = 2;
-    ctx.lineCap = "round";
-    ctx.stroke();
   }
 });
